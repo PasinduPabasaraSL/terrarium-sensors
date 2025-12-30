f51ca1d8b6b07d4e907e96812c424dfac29e3a65
PasinduPabasaraSL <pabasarapasindu365@gmail.com>
2025-12-30 10:52:16 +0530

    Remove soli moisture sensor code and Add light sensor code

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,36 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <BH1750.h>
 
-const int moisturePin = 34;
+#define SDA_PIN 26
+#define SCL_PIN 27
+
+BH1750 lightMeter;
 
 void setup() {
     Serial.begin(115200);
     delay(1000);
 
-    analogReadResolution(12);
-    Serial.println("Soil Moisture Sensor Started");
+    Wire.begin(SDA_PIN, SCL_PIN);
+
+    if (!lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE, 0x23, &Wire)) {
+        Serial.println("[BH1750] ERROR: not found");
+        while (true) delay(1000);
+    }
+
+    Serial.println("[BH1750] Ready");
 }
 
 void loop() {
-    int rawValue = analogRead(moisturePin);
+    float lux = lightMeter.readLightLevel();
 
-    Serial.print("Raw moisture value: ");
-    Serial.println(rawValue);
+    if (lux < 0) {
+        Serial.println("[BH1750] Read error");
+    } else {
+        Serial.print("Light: ");
+        Serial.print(lux);
+        Serial.println(" lx");
+    }
 
     delay(1000);
 }
